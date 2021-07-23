9f82c1473d7256ed44733bc5cb55a4183f921af1
Tom Hanks <amaresch@gmail.com>
2021-07-23 00:23:14 -0500

    added api route

---
+++ routes/api.js
@@ -0,0 +1,61 @@
+const router = require("express").Router();
+const path = require("path");
+const Workout = require("../models/Workout");
+
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .sort({ day: -1 })
+    .limit(7)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
