51968a71d4085618d4e7b39449de4f08becd4f0c
Tom Hanks <amaresch@gmail.com>
2021-07-23 00:22:09 -0500

    added workout model

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please enter exercise type",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please enter a name for the exercise",
+      },
+      duration: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+  totalDuration: {
+    type: Number,
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
